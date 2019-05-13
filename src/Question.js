f5f7b2ebe5978b3efdcc9dbb4726002f0278ba03
Erin Dustin <erincdustin@gmail.com>
2019-05-13 13:50:30 -0500

    working through Quiz and Question constructors

--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,24 @@
 class Question {
+  constructor() {
+  this.text = '',
+  this.answers = [],
+  this.correctAnswer = '',
+  this.userAnswer = '';
+  }
 
+  submitAnswer(answer) {
+    this.userAnswer = answer;
+  }
+
+  answerStatus() {
+    if (this.userAnswer && this.userAnswer === this.correctAnswer) {
+      return 1;
+    } else if (this.userAnswer && this.userAnswer !== this.correctAnswer) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
 }
 
 export default Question;
--- src/Quiz.js
+++ src/Quiz.js
@@ -10,15 +10,41 @@ class Quiz {
     // Array of Question instances
     this.asked = [];
     this.active = false;
+    this.score = 0;
+    this.scoreHistory = [];
 
     // TASK: Add more props here per the exercise
 
   }
 
-  // Example method:
   startGame() {
     this.active = true;
+    //placeholder- getAPI
   }
+  moveQuestion() {
+    this.asked.push(this.unasked.shift());
+    console.log(this.asked);
+    console.log(this.unasked);
+  }
+  updateScoreHistory() {
+    if (this.asked.length === 5) {
+      this.score.History.push(this.score);
+    }
+  }
+  endGame() {
+    this.active = false;
+  }
+
+  // updateScore() {
+  //   if (Question.userAnswer && this.userAnswer === this.correctAnswer) {
+  //     return 1;
+  //   } else if (this.userAnswer && this.userAnswer !== this.correctAnswer) {
+  //     return 0;
+  //   } else {
+  //     return -1;
+  //   }
+  // }
+
 }
 
 export default Quiz;
