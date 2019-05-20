548ad24b25e75c248a4b5fb3b68175e86945f0c7
Erin Dustin <erincdustin@gmail.com>
2019-05-20 18:20:36 -0500

    edited QuizStatus.js to remove duplicative HTML strings

--- src/QuizStatus.js
+++ src/QuizStatus.js
@@ -4,22 +4,22 @@ class QuizStatus extends Renderer {
 
   template() {
     const highScore = this.model.scoreHistory;
+    let highScoreHistory = this.model.score;
+    let progressBar = 'Inactive';
+
     if (!this.model.active && this.model.asked.length === 5) {
-      return `<div class='status-elements div-1'>Progress: Inactive</div>
-      <div class='status-elements div-2'>High score = ${Math.max(...highScore)}</div>
-      <div class='status-elements div-3'>Score = ${this.model.scoreHistory[0]}</div>`;
+      highScoreHistory= this.model.scoreHistory[0];
     }
-    else if (!this.model.active) { 
-      return `<div class='status-elements div-1'>Progress: Inactive</div>
-        <div class='status-elements div-2'>High score = ${Math.max(...highScore)}</div>
-        <div class='status-elements div-3'>Score = ${this.model.score}</div>`;
+    
+    if (this.model.active) {
+      progressBar= this.model.asked.length + ' of ' + (this.model.asked.length + this.model.unasked.length);
     }
-    else {
-      return `
-    <div class='status-elements div-1'>Progress: ${this.model.asked.length} of ${this.model.asked.length + this.model.unasked.length}</div>
+      
+    return `
+    <div class='status-elements div-1'>Progress: ${progressBar}</div>
     <div class='status-elements div-2'>High score = ${Math.max(...highScore)}</div>
-    <div class='status-elements div-3'>Score = ${this.model.score}</div>
-    `;}
+    <div class='status-elements div-3'>Score = ${highScoreHistory}</div>
+    `
   }
 }
 
